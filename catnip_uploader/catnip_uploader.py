b279ff32f956f54104dd2852c3eea5372c0d0600
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2024-01-16 16:05:52 -0500

    Upload Catnip_uploader
    - Load firmware

--- catnip_uploader/catnip_uploader.py
+++ catnip_uploader/catnip_uploader.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 import sys
 import io
@@ -14,6 +15,8 @@ GITHUB_RAW_REPO_URL = "https://raw.githubusercontent.com/ElectronicCats/CatSniff
 START_OF_FRAME = "ñ<"
 END_OF_FRAME   = ">ñ"
 TMP_FILE       = "firmware.hex"
+COMMAND_ENTER_BOOTLOADER = "ñÿ<boot>ÿñ"
+COMMAND_EXIT_BOOTLOADER = "ñÿ<exit>ÿñ"
 
 def show_ports():
     for port in list_ports.comports():
@@ -75,7 +78,7 @@ def send_ping():
         while True:
             readline = ser.read_until("\n")
             print(readline)
-            if readline != b"":
+            if readline == "BOOT":
                 break
             time.sleep(1)
         ser.close()
@@ -87,23 +90,59 @@ def send_ping():
 def send_bootloader_mode():
     try:
         ser = serial.Serial(args.port, 921600, timeout=1)
-        ser.write(create_command(b"B"))
+        ser.write(bytes(COMMAND_ENTER_BOOTLOADER.encode()))
+        print("Waiting for response:")
+        while True:
+            readline = ser.read_until("\n")
+            print(readline)
+            if readline.find("BOOT") != -1:
+                break
+            time.sleep(1)
         ser.close()
     except Exception as e:
         print("Error al enviar bootloader", e)
 
-parser = argparse.ArgumentParser(description='CatSniffer Firmware Loader', )
+def send_exit_bootloader_mode():
+    try:
+        ser = serial.Serial(args.port, 500000, timeout=1)
+        ser.write(bytes(COMMAND_EXIT_BOOTLOADER.encode()))
+        print("Waiting for response:")
+        while True:
+            readline = ser.read_until("\n")
+            print(readline)
+            if readline.find("PASSTRHOUGH") != -1:
+                break
+            time.sleep(1)
+        ser.close()
+    except Exception as e:
+        print("Error al enviar exit bootloader", e)
+
+def show_file_release():
+    request_release = requests.get("http://localhost:80/releases.json")
+    request_release.raise_for_status()
+    
+    print("Releses: ", request_release.json())
+        
+
+def show_json_releases(release):
+    string_coso = ""
+    for i in release:
+        string_coso += f"{i}: {release[i]}"
+    return string_coso
+
+parser = argparse.ArgumentParser(description='CatSniffer Firmware Loader')
 parser.add_argument('-p', '--port', help='Serial port to use', required=True)
 parser.add_argument('-f', '--firmware', help='Firmware to load', required=False)
 parser.add_argument('-v', '--version', help='Version to load', required=False)
 args = parser.parse_args()
+version = int(args.version)
 
-
+show_file_release()
 
 if args.firmware:
-    if int(args.version) == 0:
+    if version== 0:
         list_releases = list_releases_version_2
-    elif int(args.version) == 1:
+    elif version== 1:
         list_releases = list_releases_version_3
     else:
         print("Version not found")
@@ -115,8 +154,13 @@ if args.firmware:
     firmware_selected = list_releases[int(args.firmware)]
     print(firmware_selected)
 
-# is_valid = send_ping()
-# if is_valid:
-#     print("CatSniffer found")
-#     send_bootloader_mode()
+
+
+
+
+
+
+send_bootloader_mode()
+time.sleep(1)
 load_firmware()
+send_exit_bootloader_mode()
